875b31779103a2db0adf2823e64ebd66ace68fed
lpofredc <frederic.cloitre@lpo.fr>
2020-03-25 15:43:02 +0100

    update translations

--- frontend/src/app/auth/register/register.component.ts
+++ frontend/src/app/auth/register/register.component.ts
@@ -43,7 +43,7 @@ export class RegisterComponent {
               this.successMessage = null;
               this.activeModal.close();
             });
-
+            
             this.displaySuccessMessage(message);
             // redirect ?
             if (this.auth.redirectUrl) {
