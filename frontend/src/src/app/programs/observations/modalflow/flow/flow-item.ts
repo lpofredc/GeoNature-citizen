d35170ee2b889de9d822441e0bcef28240cada23
lpofredc <frederic.cloitre@lpo.fr>
2020-03-26 14:33:53 +0100

    fix dup src dir

--- frontend/src/src/app/programs/observations/modalflow/flow/flow-item.ts
+++
@@ -1,5 +0,0 @@
-import { Type } from "@angular/core";
-
-export class FlowItem {
-  constructor(public component: Type<any>, public data?: any) {}
-}
--- frontend/src/src/app/programs/observations/modalflow/flow/flow.directive.ts
+++
@@ -1,8 +0,0 @@
-import { Directive, ViewContainerRef } from "@angular/core";
-
-@Directive({
-  selector: "[user-flow]"
-})
-export class FlowDirective {
-  constructor(public viewContainerRef: ViewContainerRef) {}
-}
