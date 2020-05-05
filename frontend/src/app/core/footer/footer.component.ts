f3cddd98be5d9e6affe755b9810a8cbe7cbfc1c3
lpofredc <frederic.cloitre@lpo.fr>
2020-05-05 11:04:19 +0200

    footer improvements

--- frontend/src/app/core/footer/footer.component.ts
+++ frontend/src/app/core/footer/footer.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: '../../../custom/footer/footer.html',
-  styleUrls: ['../../../custom/footer/footer.css']
+  styleUrls: ['./footer.component.css','../../../custom/footer/footer.css']
 })
 export class FooterComponent implements OnInit {
 
